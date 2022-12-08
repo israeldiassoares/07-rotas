70ab921b4e2c25265cb244c6e80016ec48ee1487
Israel Soares <israeldiassoares@gmail.com>
2022-12-08 17:36:36 +0100

    refactored centralized components imports to cursos.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,33 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
-import { AppRountingModule } from './app.routing.module'
 
 import { AppComponent } from './app.component'
 import { HomeComponent } from './home/home.component'
-import { CursosComponent } from './cursos/cursos.component'
 import { LoginComponent } from './login/login.component'
+import { CursosModule } from './cursos/cursos.module'
+import { AppRountingModule } from './app.routing.module'
+// import { CursosComponent } from './cursos/cursos.component'
 // import { rounting } from './app.routing';
-import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component'
-import { CursosService } from './cursos/cursos.service'
-import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component'
+// import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component'
+// import { CursosService } from './cursos/cursos.service'
+// import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    CursosComponent,
     LoginComponent,
-    CursoDetalheComponent,
-    CursoNaoEncontradoComponent
+    // CursosComponent,
+    // CursoDetalheComponent,
+    // CursoNaoEncontradoComponent
   ],
   imports: [
     BrowserModule,
     // rounting
+    CursosModule,
     AppRountingModule
   ],
-  providers: [ CursosService ],
+  // providers: [ CursosService ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/cursos/cursos.module.ts
@@ -0,0 +1,20 @@
+import { RouterModule } from '@angular/router'
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+
+import { CursosComponent } from './cursos.component'
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component'
+import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component'
+import { CursosService } from './cursos.service'
+
+@NgModule({
+    imports: [ CommonModule, RouterModule ],
+    exports: [],
+    declarations: [
+        CursosComponent,
+        CursoDetalheComponent,
+        CursoNaoEncontradoComponent
+    ],
+    providers: [ CursosService ],
+})
+export class CursosModule { }
